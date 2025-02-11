86ab4d2baf1eebae151dbe557ca951dcf27d2635
김준우 <moderated.coder@gmail.com>
2025-02-11 18:19:26 +0900

    final refine logic.

--- backend/refine_article_columns.py
+++ backend/refine_article_columns.py
@@ -23,12 +23,12 @@ logger.addHandler(handler)
 
 
 # load dataset
-df = pd.read_csv("./data/구로동_parsed_article.csv")
+summary_df = pd.read_csv("./data/구로동_parsed_article.csv")
 
 
 # refine
 article_list = list()
-for index, record in tqdm(enumerate(df.to_dict('records'))):
+for index, record in tqdm(enumerate(summary_df.to_dict('records'))):
     article_num = record["atclNo"]
     article_details = ast.literal_eval(record["article_details"])
 
@@ -69,5 +69,45 @@ for index, record in tqdm(enumerate(df.to_dict('records'))):
 
     article_list.append(article_record)
 
-article_df = pd.DataFrame.from_dict(article_list).fillna("None")
-article_df.to_csv("./data/refined_articles.csv")
+refined_df = pd.DataFrame.from_dict(article_list).fillna("None")
+refined_df.to_csv("./data/refined_articles.csv")
+
+
+
+target_summary_column_list = {
+    "atclNo": "article_no",
+    "atclNm": "article_title",
+    "rletTpNm": "article_class",
+    "tradTpNm": "transaction_type",
+    "flrInfo": "floor",
+    "direction": "direction",
+    "atclCfmYmd": "article_regist_date",
+    "tagList": "tag_list",
+    "spc1": "supply_area",
+    "spc2": "exclusive_area",
+}
+
+target_refined_column_map = {
+    "관리비": "management_fee",
+    "융자금": "loan",
+    "article_short_features": "article_short_features",
+    "article_short_description": "article_short_description",
+    "향": "house_direction",
+    "입주가능일": "possible_move",
+    "매물소개": "article_description_report",
+    "agent_office": "agent_office",
+    "agent_hp": "agent_hp",
+    "agent_office_post": "agent_office_post",
+    "방구조": "shape_of_house",
+    "복층여부": "duplex",
+}
+
+target_refined_df = refined_df[target_refined_column_map.keys()]
+target_summary_df = summary_df[target_summary_column_list.keys()]
+
+target_refined_df = target_refined_df.rename(columns=target_refined_column_map)
+target_summary_df = target_summary_df.rename(columns=target_summary_column_list)
+
+total_df = pd.concat([target_summary_df, target_refined_df], axis=1)
+total_df.to_csv("./data/all_info_contained_articles.csv")
+logger.warning(f"finally refine the articles: {'./data/all_info_contained_articles.csv'}")
