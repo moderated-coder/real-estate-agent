12501c169e4f894b6e38fa9ae8d6d05d486b524e
lee gyu yeol-mac <i1004gy@gmail.com>
2025-02-22 00:26:18 +0900

    webview 변경

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,9 @@ import svgr from "vite-plugin-svgr";
 import path from "path";
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
