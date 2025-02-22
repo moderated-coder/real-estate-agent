088d028e6df22d018c17b3c874cffca47731baf6
lee gyu yeol-mac <i1004gy@gmail.com>
2025-02-22 09:37:18 +0900

    Add mobile folder as a normal directory

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Platform} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const LOCAL_HOST =
+  Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+const DEPLOYED_URL = 'https://your-deployed-react-app.com'; // 실제 배포된 주소
+
+const App = () => {
+  const webviewUrl = __DEV__ ? LOCAL_HOST : DEPLOYED_URL;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView source={{uri: webviewUrl}} style={styles.webview} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default App;
