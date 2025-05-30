8381ac17ff41d69c0772141fb6d97d09cd076e32
lee gyu yeol-mac <i1004gy@gmail.com>
2025-05-30 10:48:16 +0900

    url에 보증금 월세 위치 추가

--- front-next/src/app/realestate/_components/SearchBar.tsx
+++ front-next/src/app/realestate/_components/SearchBar.tsx
@@ -4,15 +4,19 @@ import { useState, useRef } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useSearchParams, useRouter } from "next/navigation";
 import Search from "@/src/assets/icons/search.svg";
-const SearchBar = () => {
+interface props {
+  depositRange: [number, number];
+  monthlyRentRange: [number, number];
+}
+const SearchBar = ({ depositRange, monthlyRentRange }: props) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
-  const textareaRef = useRef<HTMLInputElement>(null);
+  const searchRef = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
   const searchParams = useSearchParams();
   const router = useRouter();
   // 검색 핸들러
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
+  const handleInput = () => {
+    setSearchQuery(searchRef.current?.value || "");
   };
 
   const handleSearch = () => {
@@ -20,24 +24,32 @@ const SearchBar = () => {
       alert("검색어를 입력해주세요!");
       return;
     }
-    queryClient.removeQueries({ queryKey: ["search", searchQuery] });
+    queryClient.removeQueries({
+      queryKey: ["search", searchQuery, depositRange[0], depositRange[1], monthlyRentRange[0], monthlyRentRange[1]],
+    });
     const params = new URLSearchParams(searchParams.toString());
+    console.log("searchQuery", searchQuery);
     params.set("q", searchQuery);
+    params.set("deposit_min", depositRange[0].toString());
+    params.set("deposit_max", depositRange[1].toString());
+    params.set("rent_min", monthlyRentRange[0].toString());
+    params.set("rent_max", monthlyRentRange[1].toString());
     router.replace(`?${params.toString()}`);
-    setSearchQuery(() => "");
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.nativeEvent.isComposing) return;
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
       handleSearch();
+      setSearchQuery(() => "");
     }
   };
   return (
     <div
       className="
     flex items-center justify-between
-    w-full max-w-[600px] h-[40px]
+    w-full  h-[40px]
     px-4 mx-auto mb-5
     border border-[#ddd] rounded-lg
     bg-white hover:bg-[#f5f5f5]
@@ -46,10 +58,9 @@ const SearchBar = () => {
     >
       <div className="flex justify-between flex-1">
         <input
-          className="flex w-full [font-family:Arial,_Apple_SD_Gothic_Neo,_sans-serif]
-          leading-[22px] text-[16px] border-b-8 border-transparent overflow-hidden
+          className="flex w-full text-[16px] border-b-8 border-transparent overflow-hidden
           border-none outline-none"
-          ref={textareaRef}
+          ref={searchRef}
           value={searchQuery}
           onChange={(e) => handleInput(e)}
           onKeyDown={handleKeyDown}
