c9c681d11da1a3ec0e19feeaf4163f42e883e5b5
lee gyu yeol-mac <i1004gy@gmail.com>
2025-05-31 14:33:24 +0900

    검색창 필요한 정보를 넘겨주게 변경

--- front-next/src/app/realestate/_components/SearchBar.tsx
+++ front-next/src/app/realestate/_components/SearchBar.tsx
@@ -9,32 +9,33 @@ interface props {
   monthlyRentRange: [number, number];
 }
 const SearchBar = ({ depositRange, monthlyRentRange }: props) => {
-  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [searchGu, setSearchGu] = useState<string>("");
   const searchRef = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
   const searchParams = useSearchParams();
   const router = useRouter();
   // 검색 핸들러
   const handleInput = () => {
-    setSearchQuery(searchRef.current?.value || "");
+    setSearchGu(searchRef.current?.value || "");
   };
 
   const handleSearch = () => {
-    if (!searchQuery.trim()) {
+    if (!searchGu.trim()) {
       alert("검색어를 입력해주세요!");
       return;
     }
     queryClient.removeQueries({
-      queryKey: ["search", searchQuery, depositRange[0], depositRange[1], monthlyRentRange[0], monthlyRentRange[1]],
+      queryKey: ["search", searchGu, depositRange[0], depositRange[1], monthlyRentRange[0], monthlyRentRange[1]],
     });
     const params = new URLSearchParams(searchParams.toString());
-    console.log("searchQuery", searchQuery);
-    params.set("q", searchQuery);
+    params.set("gu", searchGu);
     params.set("deposit_min", depositRange[0].toString());
     params.set("deposit_max", depositRange[1].toString());
     params.set("rent_min", monthlyRentRange[0].toString());
     params.set("rent_max", monthlyRentRange[1].toString());
     router.replace(`?${params.toString()}`);
+    setSearchGu(() => "");
+    searchRef.current!.value = "";
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -42,7 +43,6 @@ const SearchBar = ({ depositRange, monthlyRentRange }: props) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
       handleSearch();
-      setSearchQuery(() => "");
     }
   };
   return (
@@ -61,10 +61,10 @@ const SearchBar = ({ depositRange, monthlyRentRange }: props) => {
           className="flex w-full text-[16px] border-b-8 border-transparent overflow-hidden
           border-none outline-none"
           ref={searchRef}
-          value={searchQuery}
-          onChange={(e) => handleInput(e)}
+          value={searchRef.current?.value || ""}
+          onChange={handleInput}
           onKeyDown={handleKeyDown}
-          placeholder="찾고 싶은 부동산 정보를 입력해주세요"
+          placeholder="찾고 싶은 지역 구 정보를 입력해주세요"
         />
         <Search onClick={handleSearch} style={{ width: "30px" }} />
       </div>
