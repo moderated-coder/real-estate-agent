93bd823ed204e0f7f76a85b650a60136083fcfbf
lee gyu yeol-mac <i1004gy@gmail.com>
2025-06-03 23:18:11 +0900

    타입 지정과 부동산 데이터 가져오고 폴더 추가와 폴더 가져오는 api

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from typing import Dict, Any
+from typing import Dict, Any,Optional
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
+from fastapi import Body, APIRouter
 from contextlib import asynccontextmanager
 from service.gu_service import GuService
 from service.article_read_service import ArticleReadService
@@ -51,13 +52,10 @@ async def get_article(filter_condition_list: Dict[str, Any]):
 async def test_get_dongname_list(guname: str):
     return gu_service._get_dongname_unit_code_list(guname)
 
-
-
-
-
 @app.get("/get_articles")
 async def get_articles(
     gu: str = Query(..., description="정렬 기준 (예: deposit_fee_asc)"),
+    dong: Optional[str] = Query(None, description="동 이름 (선택 사항)"),
     deposit_min: int = Query(..., description="최소 보증금"),
     deposit_max: int = Query(..., description="최대 보증금"),
     rent_min: int = Query(..., description="최소 월세"),
@@ -66,12 +64,24 @@ async def get_articles(
 ):
     return gu_service.get_articles(
         gu=gu,
+        dong=dong,
         deposit_min=deposit_min,
         deposit_max=deposit_max,
         rent_min=rent_min,
         rent_max=rent_max,
         cursor=cursor,
     )
+@app.post("/save/estate")
+async def save_estate(estate_id: Dict[str, Any] = Body(..., description="저장할 부동산 데이터 id")):
+    return gu_service.save_estate(estate_id)
+
+@app.post("/folder/create")
+async def create_folder(user_id: str = Body(..., description="사용자 ID"), folder_name: str = Body(..., description="생성할 폴더 이름")):
+    return gu_service.create_folder(user_id, folder_name)
+
+@app.get("/folder/list")
+async def get_folder_list(user_id: str = Query(..., description="사용자 ID")):
+    return gu_service.get_folder_list(user_id)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
