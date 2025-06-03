a508d8760bf8635d48f4d63919424becb10d92fb
lee gyu yeol-mac <i1004gy@gmail.com>
2025-06-04 00:17:28 +0900

    커밋 실험

--- front-next/src/app/layout.tsx
+++ front-next/src/app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body suppressHydrationWarning>
+        <div></div>
         <RQProviders>
           <main className="max-w-6xl mx-auto px-4 pt-4 pb-10">{children}</main>
         </RQProviders>
