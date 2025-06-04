e52d444dacfdfde9357d2d9565c9c3b62547283d
lee gyu yeol-mac <i1004gy@gmail.com>
2025-06-04 19:24:50 +0900

    cors 배포 주소로 변경

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://real-estate-front-wwqr.vercel.app/"],
+    allow_origins=["https://real-estate-front-wwqr.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
