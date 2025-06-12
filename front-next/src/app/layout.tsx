eab4b4e491ba7fdc1a18a37bb71f672171c6991c
lee gyu yeol-mac <i1004gy@gmail.com>
2025-06-12 15:22:27 +0900

    css 변경

--- front-next/src/app/layout.tsx
+++ front-next/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import RQProviders from "./_components/RQProvider";
 import { Analytics } from "@vercel/analytics/next";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,7 +10,7 @@ export default function RootLayout({
     <html lang="ko">
       <body suppressHydrationWarning>
         <RQProviders>
-          <main className="max-w-6xl mx-auto px-4 pt-4 pb-10">{children}</main>
+          <main className="mx-auto px-4 pt-4 pb-10">{children}</main>
           <Analytics />
         </RQProviders>
       </body>
